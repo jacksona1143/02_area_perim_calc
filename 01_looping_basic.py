c7404cda1b3a8c158236e9845bec2a6ae1ec1491
jacksona1143 <101374259+jacksona1143@users.noreply.github.com>
2022-03-25 14:37:58 +1300

    Update 01_looping_basic.py

--- 01_looping_basic.py
+++ 01_looping_basic.py
@@ -1,5 +1,19 @@
+
+
+
 valid = False
 
+error = print("Please enter a value more than 0")
 while not valid:
-    print("this is a loop")
-    keep_going= input("again? ")
+    try:
+        response = float(input("Enter a number: "))
+
+        if response > 0:
+            valid = True
+
+        else:
+                print(error)
+                print()
+
+    except ValueError:
+        print(error)
