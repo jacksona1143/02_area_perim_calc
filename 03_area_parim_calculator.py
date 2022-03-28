1a144c26380b5b999deb362382ffebac85203729
jacksona1143 <101374259+jacksona1143@users.noreply.github.com>
2022-03-29 12:00:04 +1300

    Create 03_area_parim_calculator.py

---
+++ 03_area_parim_calculator.py
@@ -0,0 +1,41 @@
+
+from re import A
+#functions
+
+#check if number is greater than zero
+def num_check (question):
+    valid = False
+    while not valid: 
+        
+        error = "Please enter a value more than 0"
+
+        try:
+            response = float(input(question))
+
+            if response > 0:
+                return response
+
+            else:
+                    print(error)
+
+        except ValueError:
+                        print(error)
+
+       
+        
+        
+
+         
+
+
+#code runs here
+
+    width = num_check ( "Width: ")
+    height = num_check ( "Height: ")
+    print()
+    print("Width", width)
+    print("Height", height)
+    print()
+            
+
+        
