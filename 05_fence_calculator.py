b97009e10fca3ddb76c8a82cbcba5893dd0c3ad4
jacksona1143 <101374259+jacksona1143@users.noreply.github.com>
2022-05-12 09:59:14 +1200

    Update 05_fence_calculator.py

--- 05_fence_calculator.py
+++ 05_fence_calculator.py
@@ -39,11 +39,15 @@ while keep_going == "":
 
     # call your number checker function three times to get the 
     # width, length and cost_per_m of the fencing
-    print("replace me with input statements")
+    width = num_check ( "Width: ")
+    height = num_check ( "Height: ")
+    cost_per_m = num_check("Cost Per Metre: $")
 
     # Calulate perimeter (width + height) x 2
+    perimeter = (width + height)* 2
     
     # Calculate the cost of the fencing (perimeter x price / meter)
+    cost_of_fencing = (perimeter * cost_per_m * 2)
 
     # Output the perimeter and cost of the fencing
 
